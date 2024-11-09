c13d2659133702e0f6435d6206ba29822eaac64c
Oluwatunmise Dunmade <oluwatunmisedunmade2017@gmail.com>
2024-11-09 09:57:19 +0100

    Initial Commit

---
+++ Guess the flag/Guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_flagApp.swift
+//  Guess the flag
+//
+//  Created by FlexOff on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
